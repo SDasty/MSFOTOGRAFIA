c5cd5f36a145e331ecda58e9a5c9635e77ab7a01
SDasty <danielstivensrq@gmail.com>
2024-01-27 13:05:20 -0500

    heroku settings

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 
