62519ffaae7b3198cccc71affc24a9542add6ebc
SDasty <danielstivensrq@gmail.com>
2024-01-27 14:36:22 -0500

    heroku settings

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -12,13 +12,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -125,11 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS = [
  os.path.join(BASE_DIR, "static"),
  'moviereviews/static/',
 ]
+
+import django_heroku
+django_heroku.settings(locals())
 
 LOGIN_URL = 'loginaccount'
